7ed96da621011741ea6491f2340660b497a50e68
Mehdqtx <Mehdi.delvaux@gmail.com>
2015-11-19 17:34:35 +0100

    Update Reservation.py

--- Reservation.py
+++ Reservation.py
@@ -2,12 +2,13 @@ import EnsReservation
 
 class Reservation :
 
-    def __init__(self,Reservation_id = None, user_id = None, Exemplaire_id = None,Reservation_date = None) :
+    def __init__(self,Reservation_id = None, user_id = None,Jeu_id = None, Exemplaire_id = None,date_Reservation = None) :
     
         self.Reservation_id = Reservation_id
         self.user_id = user_id
+        self.Jeu_id = Jeu_id
         self.Exemplaire_id = Exemplaire_id
-        self.Reservation_date = Reservation_date
+        self.date_Reservation = date_Reservation
     
     
   ### GETTERS###
@@ -15,21 +16,24 @@ class Reservation :
     def get_Reservation_id(self):
         return self.Reservation_id
     def get_user_id(self):
-        return self.user_id    
+        return self.user_id   
+    def get_Jeu_id(self)
+        return self.Jeu_id
     def get_Exemplaire_id(self):
         return self.Exemplaire_id
-    def get_Reservation_date(self):
-        return self.Reservation_date
+    def get_date_Reservation(self):
+        return self.date_Reservation
         
     ##### SETTERS #####
     
     def set_user_id(self, user_id):
         self.user_id = user_id
+    def set_Jeu_id(self, Jeu_id):
+        self.Jeu_id = Jeu_id
     def set_Exemplaire_id(self, Exemplaire_id):
         self.Exemplaire_id = Exemplaire_id
-    def set_Reservation_date(self, Reservation_date):
-        self.Reservation_date = Reservation_date
-           
+    def set_Reservation_date(self, date_Reservation):
+        self.date_Reservation = date_Reservation
         
   ##### FONCTIONS ANNEXES #####
   
