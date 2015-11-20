9412cd1909a95d5624021b4dccd97631c44d292c
Mehdqtx <mehdi.delvaux@gmail.com>
2015-11-20 14:51:32 +0100

    changement

--- EnsReservation.py
+++ EnsReservation.py
@@ -1,3 +1,4 @@
+#-*-coding: utf-8 -*-
 import sqlite3
 from Extension import Extension
 from Jeu import Jeu 
@@ -10,22 +11,23 @@ conn.commit()
 cur = conn.cursor()
 
 def createTable():
-        cur.execute("""CREATE TABLE IF NOT EXISTS EnsReservation(
-                        Reservation_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-                        user_id INTEGER,
-                        Jeu_id INTEGER,
-                        Extension_id INTEGER,
-                        Exemplaire_id INTEGER,
-                        date_Reservation VARCHAR,
-                        
-                                        )""")
-                                        
-        conn.commit()
-        
-        
+	cur.execute("""CREATE TABLE IF NOT EXISTS EnsReservation(
+			Reservation_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+			user_id INTEGER,
+			Jeu_id INTEGER,
+			Extension_id INTEGER,
+			Exemplaire_id INTEGER,
+			date_Reservation DATE,
+			Terminer BOOLEAN,
+			
+					)""")
+					
+	conn.commit()
+	
+	
 def destroyTable():
-        cur.execute("""DROP TABLE EnsReservation""")
-        conn.commit()
+	cur.execute("""DROP TABLE EnsReservation""")
+	conn.commit()
 
 def Reservation_EnCours(User) :
 
@@ -33,13 +35,22 @@ def Reservation_EnCours(User) :
 	result=cur.fetchone()
 	return result != None
 
-def Ajouter_Reservation(User):
-        #Ajouter_Reservation : Reservation x Utilisateur x EnsReservation -> EnsReservation
-   if (not(Reservation_EnCours(User))):
+def get_Reservation(User) :
+
+	cur.execute=(""" SELECT * FROM EnsReservation WHERE user_id = ? """, (Reservation.get_user_id()))
+	result = cur.fetchall()
+
+	for ReservationCur in result :
+		ReservationCur = Reservation(ReservationCur[0],ReservationCur[1],ReservationCur[2],ReservationCur[3],ReservationCur[4],ReservationCur[5],ReservationCur[6])
+	
+
+def Ajouter_Reservation(Reservation):
+	#Ajouter_Reservation : Reservation x Utilisateur x EnsReservation -> EnsReservation
+	if (not(Reservation_EnCours(User))):
 		try:
 			cur.execute=(""" INSERT INTO EnsReservation(Reservation_id, Jeu_id, user_id, Exemplaire_id, date_Reservation) VALUES(?, ?, ?, ?, ?) """, (Reservation.get_Reservation_id(), Reservation.get_Jeu_id(), Reservation.get_user_id(), Extension.get_date_Reservation() ))
 			conn.commit()
-			 print(" Reservation ajoutée !")
+			print(" Reservation ajoutée !")
 		except:
 			print ("Erreur lors de l'ajout d'une reservation")
 
@@ -48,9 +59,9 @@ def Ajouter_Reservation(User):
 		
 		
 def supprimer_Reservation(Reservation):
-        #Supprime une reservation
+	#Supprime une reservation
 	try:
-		cur.execute=(""" DELETE FROM EnsReservation WHERE Reservation_id = ?""", (Reservation.get_Reservation_id()))
+		cur.execute=(""" DELETE FROM EnsReservation WHERE Reservation_id = ?""", (Reservation.get_user_id()))
 		conn.commit()
 		
 	except:
@@ -58,21 +69,21 @@ def supprimer_Reservation(Reservation):
 		
 		
 def Nombre_De_Reservation():
-        # nombre_Reservation: EnsReservation -> Entier, renvoie le nombre de Reservation . """
+	# nombre_Reservation: EnsReservation -> Entier, renvoie le nombre de Reservation . """
 
 	cur.execute =(""" SELECT COUNT (Reservation_id) FROM EnsReservation""")
 	result = cur.fetchone()
 	return result[0]
 
-def rechercher_Reservation_User(user_id):
+def rechercher_Reservation_User(User):
        #rechercher_Reservation_User: Int -> EnsReservation, renvoie la reservation de l'utilisateur que l'on veut connaitre.
 	
-        cur.execute("""SELECT * FROM EnsReservation WHERE user_id LIKE ?""",(user_id,))
-        Reservaton_user = cur.fetchall()
-        return Reservaton_user
+	cur.execute("""SELECT * FROM EnsReservation WHERE user_id LIKE ?""",(user_id,))
+	Reservaton_user = cur.fetchall()
+	return Reservaton_user
 	
 def Reservation_to_table(Reservation):
-        # Reservation -> List
-        ReservationTable=(Reservation.get_Reservation_id(),Reservation.get_user_id(),Reservation.get_Jeu_id(),Reservation.get_Exemplaire_id(),Reservation.get_date_Reservation())
-        return ReservationTable
+	# Reservation -> List
+	ReservationTable=(Reservation.get_Reservation_id(),Reservation.get_user_id(),Reservation.get_Jeu_id(),Reservation.get_Exemplaire_id(),Reservation.get_date_Reservation())
+	return ReservationTable
 		
--- Reservation.py
+++ Reservation.py
@@ -2,13 +2,15 @@ import EnsReservation
 
 class Reservation :
 
-    def __init__(self,Reservation_id = None, user_id = None,Jeu_id = None, Exemplaire_id = None,date_Reservation = None) :
+    def __init__(self,Reservation_id = None, user_id = None,Jeu_id = None, Extension_id = None ,Exemplaire_id = None,date_Reservation = None, Terminer = False) :
     
         self.Reservation_id = Reservation_id
         self.user_id = user_id
         self.Jeu_id = Jeu_id
+        self.Extension_id = Extension_id
         self.Exemplaire_id = Exemplaire_id
         self.date_Reservation = date_Reservation
+        self.Terminer = Terminer
     
     
   ### GETTERS###
@@ -23,6 +25,10 @@ class Reservation :
         return self.Exemplaire_id
     def get_date_Reservation(self):
         return self.date_Reservation
+    def get_Extension_id(self):
+        return self.Extension_id
+    def get_Terminer(self):
+        return self.Terminer
         
     ##### SETTERS #####
     
@@ -34,6 +40,10 @@ class Reservation :
         self.Exemplaire_id = Exemplaire_id
     def set_Reservation_date(self, date_Reservation):
         self.date_Reservation = date_Reservation
+    def set_Terminer(self) :
+        self.Terminer = True
+    def set_Extension_id(self, Extension_id) :
+        self.Extension_id = Extension_id
         
   ##### FONCTIONS ANNEXES #####
   
