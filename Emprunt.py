4cb63ad5f2adb09453eb18194c4e52e6ccbbcc8d
JohnSnoow <jean-bezet@orange.fr>
2015-12-03 23:45:08 +0100

    Ajout EnsEmprunt et Emprunt
    ajout de la recherche sur EnsEmprunt, et des fonctions manquantes sur
    Emprunt

--- Emprunt.py
+++ Emprunt.py
@@ -9,17 +9,18 @@ class Emprunt : #Donne les infos concernant un emprunt
 		-son échéance
 		-son statut (en retard ou à l'heure)
 		-sa validité  """
-	def __init__(self, Adherent, Jeu, date_emprunt=date.today()):
-		self.adherent = Adherent  			#Nom de l'adhérent
-		self.jeu = Jeu 						#Nom du jeu emprunté
-		self.date_emprunt = date_emprunt 	#Date d'emprunt sous la forme de tableau [j(j), m(m), aaaa]
+	def __init__(self, Emprunt_id = None, Adherent_id, Jeu_id, date_emprunt=date.today()):
+		self.emprunt_id = Emprunt_id   		#Id de l'emprunt
+		self.adherent_id = Adherent_id  			#Id de l'adhérent
+		self.jeu_id = Jeu 						#Id du jeu emprunté
+		self.date_emprunt = date_emprunt	
 		self.date_echeance = calcul_echeance(self)
 
-	def get_adherent (self):
-		return(self.adherent)
+	def get_adherent_id (self):
+		return(self.adherent_id)
 
-	def get_jeu (self):
-		return(self.jeu)
+	def get_jeu_id (self):
+		return(self.jeu_id)
 
 	def get_date_emprunt (self):
 		return(self.date_emprunt)
@@ -30,11 +31,37 @@ class Emprunt : #Donne les infos concernant un emprunt
 
 	def calcul_Date_Echeance(self):
 		""" calcul_date_echeance: Emprunt -> Date, renvoie la date d'echeance de l'emprunt"""
-		new_Day = int(self.get_date_emprunt().day + 7)
+		""" nouvelle date d'échance : 3 semaines """
+		new_Day = int(self.get_date_emprunt().day + 21)
 		date_echeance=(self.get_date_emprunt()).replace(day=new_Day)
 		return date_echeance
 
+	def emprunt_En_Retard(self, date_retour):
+		""" emprunt_En_Retard: Emprunt x Date -> Bool, True si le retour de l'emprunt dépasse la date d'echeance, False sinon """
+		# Une date est considérée comme inférieure à une autre lorsqu'elle la précède dans le temps. cf doc python > date object
+		if(date_retour <= self.calcul_Date_Echeance()):
+			return True
+		else:
+			return False
 
+	def calcul_retard (self):
+		""" calcul_retard : Emprunt -> entier, 0 si emprunt_En_Retard est False, la difference entre date_retour et date_echeance sinon"""
+		date_retour = datetime.today()
+		if emprunt_En_Retard(self, date_retour):
+			jour_de_retard = date_retour - date_echeance
+		else : 
+			return 0
+
+	def emprunt_valide(self, adherent_id):
+		""" emprunt_valide : Emprunt x Adherent -> Bool, renvoie True si l'utilisateur a les droits d'emprunt et qu'il n'a pas d'emprunt en cours, False sinon"""
+		User = EnsUtilisateurs.get_user(adherent_id,)
+		return User.empruntEnCours == False
+		""" Il manque le droit d'emprunt dans utilisateurs, que faire si il y a une reservation en cours (conflit de dates)"""
+
+
+
+
+"""
 	def calcul_echeance (self):
 		date_emprunt = self.date_emprunt
 		date_echeance = []						#Date de retour. gère les années bissectiles de **** et les mois.
@@ -70,22 +97,13 @@ class Emprunt : #Donne les infos concernant un emprunt
 		date_echeance.append(mois_de_retour)
 		date_echeance.append(annee_de_retour)
 		return (date_echeance)
+"""
 
 
-	def emprunt_En_Retard(self, dateRetour):
-		""" emprunt_En_Retard: Emprunt x Date -> Bool, True si le retour de l'emprunt dépasse la date d'echeance, False sinon """
-		# Une date est considérée comme inférieure à une autre lorsqu'elle la précède dans le temps. cf doc python > date object
-		if(dateRetour <= self.calcul_Date_Echeance()):
-			return True
-		else:
-			return False
-
+"""
 	def est_en_retard (self, date_retour):
 		date_echeance = self.date_echeance
 		retard = (date_retour[0] > date_echeance[0] and date_retour[1] == date_echeance[1]) or (date_retour[1] > date_echeance[1]) or (date_retour[2] > date_echeance[2])
 		 #si on rend trop tard retard = True
 		return (retard)
-"""
-	def calcul_retard (self):
-
 """
--- EnsEmprunt.py
+++ EnsEmprunt.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import date, datetime
 
 
 conn = sqlite3.connect("Ludotheque.db")
@@ -11,20 +12,52 @@ def createTable():
                         emprunt_id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
                         user_id INTEGER,
                         jeu_id INTEGER,
-                        date_emprunt STRING)""")
+                        date_emprunt DATE,
+                        date_echeance DATE)""")  #DATE est représenté comme : YYYY-MM-DD
         conn.commit()
 
 def destroyTable():
         cur.execute("""DROP TABLE EnsEmprunt""")
         conn.commit()
-def emprunt_to_table(Emprunt):
-		emprunt_table = (Emprunt.get_emprunt_id(), Emprunt.get_adherent(), Emprunt.get_jeu_id(), Emprunt.get_date_emprunt())
+def emprunt_to_table(self, Emprunt):
+		emprunt_table = (Emprunt.get_emprunt_id(), Emprunt.get_adherent_id(), Emprunt.get_jeu_id(), Emprunt.get_date_emprunt())
 		return (emprunt_table)
 
-def delete_emprunt(Emprunt):
+def delete_emprunt(self, Emprunt):
         cur.execute("""DELETE FROM EnsEmprunt WHERE emprunt_id = ?""",(Emprunt.get_emprunt_id(),))
         conn.commit()
 
-def insert_emprunt(Emprunt):
-		cur.execute("""INSERT INTO EnsEmprunt(emprunt_id,user_id,jeu_id,date_emprunt) VALUES (?, ?, ?, ?)""",emprunt_to_table(Emprunt))
-                        conn.commit()
+def insert_emprunt(self, Emprunt):
+	cur.execute("""INSERT INTO EnsEmprunt(emprunt_id,user_id,jeu_id,date_emprunt) VALUES (?, ?, ?, ?)""",emprunt_to_table(Emprunt))
+        conn.commit()
+
+def rechercher_emprunt (self, Emprunt_id):
+        cur.execute("""SELECT * FROM EnsEmprunt WHERE emprunt_id = Emprunt_id""")
+        res = cur.fetchone()
+        return res
+
+def rechercher_user (self, User_id):
+        cur.execute("""SELECT * FROM EnsEmprunt WHERE user_id = User_id""" )
+        res = cur.fetchone()
+        return res
+
+def rechercher_jeu (self, Jeu_id):
+        cur.execute("""SELECT * FROM EnsEmprunt WHERE jeu_id = Jeu_id""")
+        res = cur.fetchall()
+        return res
+
+def rechercher_date_emprunt (self, Date_emprunt):
+        cur.execute("""SELECT * FROM EnsEmprunt WHERE date_emprunt = Date_emprunt""")
+        res = cur.fetchall()
+        return res
+
+def rechercher_date_echeance (self, Date_echeance):
+        cur.execute("""SELECT * FROM EnsEmprunt WHERE date_echeance = Date_echeance""" )
+        res = cur.fetchall()
+        return res
+
+
+"""
+date = str( year + "-"+month+"-"+day);
+date = date (date)
+"""
