dacf2b87d060a5964c0a7b2be6191eba2100346f
Darkyler <Darkyler57@gmail.com>
2015-12-04 00:46:00 +0100

    Commentaires
    Cool ! :)

--- Emprunt.py
+++ Emprunt.py
@@ -9,6 +9,7 @@ class Emprunt : #Donne les infos concernant un emprunt
 		-son échéance
 		-son statut (en retard ou à l'heure)
 		-sa validité  """
+	# Sa validité ??
 	def __init__(self, Emprunt_id = None, Adherent_id, Jeu_id, date_emprunt=date.today()):
 		self.emprunt_id = Emprunt_id   		#Id de l'emprunt
 		self.adherent_id = Adherent_id  			#Id de l'adhérent
@@ -16,6 +17,7 @@ class Emprunt : #Donne les infos concernant un emprunt
 		self.date_emprunt = date_emprunt	
 		self.date_echeance = calcul_echeance(self)
 
+	# A Rajouter: Et l'plus important : get_emprunt_id(self) 
 	def get_adherent_id (self):
 		return(self.adherent_id)
 
@@ -32,10 +34,13 @@ class Emprunt : #Donne les infos concernant un emprunt
 	def calcul_Date_Echeance(self):
 		""" calcul_date_echeance: Emprunt -> Date, renvoie la date d'echeance de l'emprunt"""
 		""" nouvelle date d'échance : 3 semaines """
+		# Est-ce que ça incrémente le mois si JJ > 10 ?
 		new_Day = int(self.get_date_emprunt().day + 21)
 		date_echeance=(self.get_date_emprunt()).replace(day=new_Day)
 		return date_echeance
-
+	
+	# Si la date de retour est inférieure: Renvoyer False => A inverser
+	# Mettre sur une seule ligne :: return (date_retour > self.get_date_echeance())
 	def emprunt_En_Retard(self, date_retour):
 		""" emprunt_En_Retard: Emprunt x Date -> Bool, True si le retour de l'emprunt dépasse la date d'echeance, False sinon """
 		# Une date est considérée comme inférieure à une autre lorsqu'elle la précède dans le temps. cf doc python > date object
@@ -51,7 +56,8 @@ class Emprunt : #Donne les infos concernant un emprunt
 			jour_de_retard = date_retour - date_echeance
 		else : 
 			return 0
-
+	
+	# ???? Mauvais type pour cette fonction, on en discute demain
 	def emprunt_valide(self, adherent_id):
 		""" emprunt_valide : Emprunt x Adherent -> Bool, renvoie True si l'utilisateur a les droits d'emprunt et qu'il n'a pas d'emprunt en cours, False sinon"""
 		User = EnsUtilisateurs.get_user(adherent_id,)
