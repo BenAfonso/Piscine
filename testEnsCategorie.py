0481a2dea6ab1c3fa14e043f45ba7b571793fee6
Jean BEZET <jean.bezet@igtp4-mac03.local>
2016-01-04 12:25:11 +0100

    Debut test unitaire

---
+++ testEnsCategorie.py
@@ -0,0 +1,12 @@
+import EnsCategories
+
+#creation d'une table EnsCategories
+createTable()
+
+#ajout d'une categorie
+ajouterCategorie(Action)
+
+#ajout d'une categorie sans nom, cela ne devrait pas marcher et lever une erreur
+ajouterCategorie()
+
+#
